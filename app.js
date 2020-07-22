9a3224c269f7a8a7ad4bb57a90521606aeaf5040
Calvin Sung <calvinsungvin@gmail.com>
2020-07-22 18:10:46 +0800

    project in

---
+++ app.js
@@ -0,0 +1,84 @@
+// 載入 express 並建構應用程式伺服器
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const Todo = require('./models/todo')
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', ()=>{
+  console.log('mongodb connected!')
+})
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// 設定首頁路由
+app.get('/', (req, res) => {
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+//the data is gonna be sent to /todos
+app.post("/todos", (req, res) => {
+  const name = req.body.name
+  return Todo.create({name})
+             .then(() => res.redirect('/'))
+             .catch(error => console.log(error))
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(()=> res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+             .then(todo => todo.remove())
+             .then(() => res.redirect('/'))
+             .catch(error => console.log(error))
+})
+
+// 設定 port 3000
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
